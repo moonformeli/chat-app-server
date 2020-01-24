9592bcfc37219707df39399c9b293df9b033e4d9
moonformeli <mgyang95@gmailc.om>
2020-01-24 23:09:52 +0900

    장만월 데이터 보완

--- src/fixtures/user/users.ts
+++ src/fixtures/user/users.ts
@@ -8,24 +8,31 @@ const users: IUser[] = [
     username: '장만월 사장님',
     messages: [
       {
-        isMe: true,
+        isMe: false,
         message: '출근했니?',
         createdAt: format('2020-01-24 09:13:22'),
-        isRead: false,
+        isRead: true,
         messageType: 'string'
       },
       {
-        isMe: true,
+        isMe: false,
         message: '출근했냐구?',
         createdAt: format('2020-01-24 09:15:06'),
-        isRead: false,
+        isRead: true,
+        messageType: 'string'
+      },
+      {
+        isMe: false,
+        message: '어딘데 출근 안하니, 죽고싶니?',
+        createdAt: format('2020-01-24 09:15:06'),
+        isRead: true,
         messageType: 'string'
       },
       {
         isMe: true,
-        message: '어딘데 출근 안하니, 죽고싶니?',
-        createdAt: format('2020-01-24 09:15:06'),
-        isRead: false,
+        message: '해외 출장중입니다.',
+        createdAt: format('2020-01-24 10:32:13'),
+        isRead: true,
         messageType: 'string'
       }
     ]
